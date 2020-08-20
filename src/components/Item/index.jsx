c09a07ddab00024fa1478e0ee501e02b432707a2
Luiz Gustavo <gugamatias7@gmail.com>
2020-08-20 13:04:16 -0300

    Adding the removed bolean in Item to differ

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -13,6 +13,10 @@ import styles from './styles';
  * @params
  *
  */
+
+function removedButton() {}
+
+function notRemovedButton() {}
 export default function Item({
   itemId,
   timeToRemember,
@@ -24,8 +28,63 @@ export default function Item({
   visibleModal,
   confirmButton,
   deleteButton,
+  removed,
 }) {
-  return (
+  return removed ? (
+    <View style={styles.wrapper}>
+      {/* BEGIN ITEM CONTENT */}
+      <TouchableOpacity onPress={handlerModal}>
+        {/* BEGIN ITEM HEADER */}
+        <ItemHeader
+          title={`In ${timeToRemember} minutes`}
+          iconName="md-time"
+          color="#195C92"
+          fontSize={30}
+        />
+        {/* END ITEM HEADER */}
+
+        {/* BEGIN ITEM BODY */}
+        <ItemBody style={styles.itemBodyContainer}>
+          {/*<Text>{itemId}</Text>*/}
+          <View style={styles.titleTextContainer}>
+            <Button
+              iconFamily="MaterialCommunityIcons"
+              iconName="format-text"
+              iconSize={30}
+              iconColor="#ffffff"
+            />
+            <Text style={styles.descriptionTextTitle}>{titleOfMemory}</Text>
+          </View>
+          <View style={styles.dateTextContainer}>
+            <Button
+              iconFamily="MaterialCommunityIcons"
+              iconName="update"
+              iconSize={20}
+              iconColor="#ffffff"
+              style={{ marginLeft: 5 }}
+            />
+            {/* [] O QUE FAZER --> Criado em {date(dd/mm/aa)} às {time(hh/mm)} */}
+            <Text style={styles.dateText}>{`${createdAtDate} ${createdAtHours}`}</Text>
+          </View>
+        </ItemBody>
+
+        {/* END ITEM BODY */}
+      </TouchableOpacity>
+      {/* BEGIN BUTTONS CONTAINER */}
+      <View style={[styles.buttonsContainerItem]}>
+        <Button
+          iconFamily="EvilIcons"
+          iconName="trash"
+          iconSize={35}
+          iconColor="red"
+          style={{ marginBottom: 9 }}
+          onPress={deleteButton}
+        />
+      </View>
+      {/* END BUTTONS CONTAINER */}
+      {/* END ITEM CONTENT */}
+    </View>
+  ) : (
     <View style={styles.wrapper}>
       {/* BEGIN ITEM CONTENT */}
       <TouchableOpacity onPress={handlerModal}>
@@ -74,14 +133,6 @@ export default function Item({
           iconColor="green"
           style={{ marginBottom: 9 }}
           onPress={confirmButton}
-        />
-        <Button
-          iconFamily="EvilIcons"
-          iconName="trash"
-          iconSize={35}
-          iconColor="red"
-          style={{ marginBottom: 9 }}
-          onPress={deleteButton}
         />
       </View>
       {/* END BUTTONS CONTAINER */}
