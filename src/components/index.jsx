99fb925445391352b6f3c2ff5092229b29a0e3d4
Luiz Gustavo <gugamatias7@gmail.com>
2020-08-20 13:03:06 -0300

    Adding the ItemHeader in the index of components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,6 +4,7 @@ import Item from './Item';
 import Modal from './Modal';
 import Input from './Input';
 import ItemModal from './ItemModal';
+import ItemHeader from './Item/ItemHeader';
 import FastRefreshModal from './FastRefreshModal';
 
-export { Button, Header, Item, Modal, Input, ItemModal, FastRefreshModal };
+export { Button, Header, Item, Modal, Input, ItemModal, ItemHeader, FastRefreshModal };
